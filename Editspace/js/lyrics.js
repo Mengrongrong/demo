5a624844d4f8c8560e2673d01cf44e1417bdd2f2
Mengrongrong <18860872083@qq.com>
2018-09-24 19:46:18 +0800

    小米官网轮播图

--- Editspace/js/lyrics.js
+++
@@ -1,8 +0,0 @@
-var songData = [{
-    songName:'Artist',
-	singer:'小鬼',
-	songSrc:'music/0.mps',
-	songImgSrc:'music/0.jpg',
-	songLyrics:'[00:00.31]sky-Beyond[00:05.51]you are my warm glaves[00:10.12]my cold beer[00:15.32]my sun-kissed shirt[00:20.12]my day after day dream[00:25.31]sky-Beyond[00:30.51]you are my warm glaves[00:35.12]my cold beer[00:40.32]my sun-kissed shirt[00:45.12]my day after day dream[00:50.32]my sun-kissed shirt[00:55.12]my day after day dream[00:60.32]my sun-kissed shirt[01:05.12]my day after day dream[01:10.31]sky-Beyond[01:15.51]you are my warm glaves[01:15.12]my cold beer[01:20.32]my sun-kissed shirt[01:25.12]my day after day dream[01:30.32]my sun-kissed shirt[01:35.12]my day after day dream',
-    }
-]
